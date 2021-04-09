3d64cbba606dc3e8ce4e59b7e4fbb7b27e01242b
withoutmeat <withoutmeat@qq.com>
2021-04-09 10:59:38 +0800

    fix: log.info -> log.default.info

--- core/cli/bin/dopa.js
+++ core/cli/bin/dopa.js
@@ -5,7 +5,7 @@ const log = require("@dopa/log");
 
 
 if (importLocal(__filename)) {
-  log.info("cli", "正在使用 immoc-cli 本地版");
+  log.default.info("cli", "正在使用 immoc-cli 本地版");
 } else {
   require("../dist/index.js").default();
 }
